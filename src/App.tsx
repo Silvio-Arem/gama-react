452d875304d964f5db15fc65f62d21161a45986e
Silvio Arem <silvio.arem@gmail.com>
2023-01-16 16:39:07 -0300

    deleting unnecessary files, cleaning application

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
-  let nome = "Silvio";
-  let classTitulo = "titulo-vermelho";
-  let titulo = <h1>Oi</h1>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Olá mundo! Eu sou {2 + 2}!
-        </p>
-        <a
-          className={classTitulo}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {titulo}
-        </a>
-      </header>
+      <h1>Pagina home</h1>
     </div>
   );
 }
