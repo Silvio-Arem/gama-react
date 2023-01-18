d6cd4a39284c08a6debd1a89e76f0973f21681c7
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 19:31:31 -0300

    pagina contato criada e estruturada

---
+++ src/pages/Contato/index.tsx
@@ -0,0 +1,10 @@
+import Header from "../../components/Header";
+
+export default function Contato() {
+    return (
+        <div>
+            <Header />
+            <h1>Página de contato</h1>
+        </div>
+    )
+}
