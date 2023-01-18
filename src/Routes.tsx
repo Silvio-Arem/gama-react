c8d6b243eb44ca25f84084bd9847201884934435
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 19:16:22 -0300

    estruturando as rotas

---
+++ src/Routes.tsx
@@ -0,0 +1,9 @@
+import { Routes as WraperRoutes, Route} from "react-router-dom";
+
+export default function Routes() {
+    return (
+        <WraperRoutes>
+            <Route  path="/" element={}/>
+        </WraperRoutes>
+    )
+}
