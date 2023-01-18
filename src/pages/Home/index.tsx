3a39c7137908b290dbd2be3da3040169dee06152
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 19:28:24 -0300

    pagina home estruturada

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,32 +3,32 @@ import Box from "../../components/Box";
 import Banner from "./assets/images/ilustra-banner.png"
 import "./styles/global.css"
 
-function App() {
+function Home() {
 
-  let sendEmail = ()=> {
-    alert("Email enviado com sucesso!");
-  }
+    let sendEmail = () => {
+        alert("Email enviado com sucesso!");
+    }
 
-  return (
-    <div className="App">
-      <Header />
-      <main>
-        <Box background="claro">
-          <div>
-            <h1> Educação Financeira é tudo de bom</h1>
-            <p>Vem comigo que te mostro por onde começar</p>
-            <button className="button" onClick={sendEmail}>quero saber mais</button>
-          </div>
-          <div>
-            <img src={Banner} alt="banner" />
-          </div>
-        </Box>
-        <Box background="escuro">
-          <p>Teste</p>
-        </Box>
-      </main>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <main>
+                <Box background="claro">
+                    <div>
+                        <h1> Educação Financeira é tudo de bom</h1>
+                        <p>Vem comigo que te mostro por onde começar</p>
+                        <button className="button" onClick={sendEmail}>quero saber mais</button>
+                    </div>
+                    <div>
+                        <img src={Banner} alt="banner" />
+                    </div>
+                </Box>
+                <Box background="escuro">
+                    <p>Teste</p>
+                </Box>
+            </main>
+        </div>
+    );
 }
 
-export default App;
+export default Home;
