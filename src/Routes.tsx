1cc98c4061062e55f38e0dd83f8106d2f4c666fd
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 19:35:28 -0300

    pagina contato importada nas rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,10 +1,12 @@
 import { Routes as WraperRoutes, Route} from "react-router-dom";
 import Home from "./pages/Home";
+import Contato from "./pages/Contato";
 
 export default function Routes() {
     return (
         <WraperRoutes>
             <Route  path="/" element={<Home />}/>
+            <Route path="/contato" element={<Contato />}/>
         </WraperRoutes>
-    )
+    );
 }
