0603f2b01a13b5d17bcd3698577af814ee82bff2
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 19:38:01 -0300

    Reestruturando o App.tsx para apontar as rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,10 @@
-import Header from "./components/Header";
-import Box from "./components/Box";
-import Banner from "./assets/images/ilustra-banner.png"
+import Routes from "./Routes";
 import "./styles/global.css"
 
 function App() {
-
-  let sendEmail = ()=> {
-    alert("Email enviado com sucesso!");
-  }
-
   return (
     <div className="App">
-      <Header />
-      <main>
-        <Box background="claro">
-          <div>
-            <h1> Educação Financeira é tudo de bom</h1>
-            <p>Vem comigo que te mostro por onde começar</p>
-            <button className="button" onClick={sendEmail}>quero saber mais</button>
-          </div>
-          <div>
-            <img src={Banner} alt="banner" />
-          </div>
-        </Box>
-        <Box background="escuro">
-          <p>Teste</p>
-        </Box>
-      </main>
+      <Routes />
     </div>
   );
 }
