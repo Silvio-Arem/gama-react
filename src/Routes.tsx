2762f3e8f7de9d7587c140fbf26a0fdb17693f88
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 19:33:39 -0300

    Home importada nas rotas

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,10 @@
 import { Routes as WraperRoutes, Route} from "react-router-dom";
+import Home from "./pages/Home";
 
 export default function Routes() {
     return (
         <WraperRoutes>
-            <Route  path="/" element={}/>
+            <Route  path="/" element={<Home />}/>
         </WraperRoutes>
     )
 }
