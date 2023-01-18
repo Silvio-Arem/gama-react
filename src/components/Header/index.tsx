7d09ed516fce99811452764ff72dde458cfe18fb
Silvio Arem <silvio.arem@gmail.com>
2023-01-18 16:53:01 -0300

    estilizando a pagina que nem a da aula

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,12 +6,16 @@ import "./styles.css";
 function Header() {
     return (
         <header>
+            <div>
             <img src={Logo} alt="Logo do dindin" />
+            </div>
+            <div>
             <nav>
                 <Link texto="Cursos" redirect="/cursos"/>
                 <Link texto="Blog" redirect="/blog"/>
                 <Link texto="Contato" redirect="/contato"/>
             </nav>
+            </div>
         </header>
     );
 }
